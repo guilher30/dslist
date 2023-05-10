f83815d1acda070ff5d66c343949193209e6536a
Guilherme Moura <mourag9899@gmail.com>
2023-05-09 22:12:15 -0300

    Busca de games por lista

---
+++ src/main/java/com/projeto/dslist/projections/GameMinProjection.java
@@ -0,0 +1,18 @@
+package com.projeto.dslist.projections;
+
+public interface GameMinProjection {
+	Long getId();
+
+	String getTitle();
+
+	Integer getYear();
+	
+	String getImgUrl();
+	
+	String getShortDescription();
+	
+	Integer getPosition();
+	
+	
+
+}
